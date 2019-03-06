d89a614f40a2f83b5eabfcd2f5a9c765197c0e34
AbhiJ2706 <43829700+AbhiJ2706@users.noreply.github.com>
2019-03-05 16:08:52 -0800

    Added hatch detection (not tested)

--- ChickenVision.py
+++ ChickenVision.py
@@ -182,6 +182,7 @@ V_FOCAL_LENGTH = image_height / (2 * math.tan((verticalView / 2)))
 # blurs have to be odd
 green_blur = 1
 orange_blur = 27
+yellow_blur = 27
 
 # define range of green of retroreflective tape in HSV
 lower_green = np.array([40, 75, 75])
@@ -189,6 +190,9 @@ upper_green = np.array([96, 255, 255])
 # define range of orange from cargo ball in HSV
 lower_orange = np.array([0, 193, 92])
 upper_orange = np.array([23, 255, 255])
+
+lower_yellow = np.array([0, 193, 92])
+upper_yellow = np.array([23, 255, 255])
 
 
 # Flip image if camera mounted upside down
@@ -270,6 +274,23 @@ def findCargo(frame, mask):
     # Processes the contours, takes in (contours, output_image, (centerOfImage)
     if len(contours) != 0:
         image = findBall(contours, image, centerX, centerY)
+    # Shows the contours overlayed on the original video
+    return image
+
+def findHatch(frame, mask):
+    # Finds contours
+    _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)
+    # Take each frame
+    # Gets the shape of video
+    screenHeight, screenWidth, _ = frame.shape
+    # Gets center of height and width
+    centerX = (screenWidth / 2) - .5
+    centerY = (screenHeight / 2) - .5
+    # Copies frame and stores it in image
+    image = frame.copy()
+    # Processes the contours, takes in (contours, output_image, (centerOfImage)
+    if len(contours) != 0:
+        image = findHatches(contours, image, centerX, centerY)
     # Shows the contours overlayed on the original video
     return image
 
@@ -369,10 +390,114 @@ def findBall(contours, image, centerX, centerY):
             currentAngleError = finalTarget[0]
             # pushes cargo angle to network tables
             networkTable.putNumber("cargoYaw", currentAngleError)
+            networkTable.putNumber("cargoDist", finalTarget[1])
 
         else:
             # pushes that it doesn't see cargo to network tables
             networkTable.putBoolean("cargoDetected", False)
+
+        cv2.line(image, (round(centerX), screenHeight), (round(centerX), 0), (255, 255, 255), 2)
+
+        return image
+
+
+def findHatches(contours, image, centerX, centerY):
+    screenHeight, screenWidth, channels = image.shape
+    # Seen vision targets (correct angle, adjacent to each other)
+    cargo = []
+
+    if len(contours) > 0:
+        # Sort contours by area size (biggest to smallest)
+        cntsSorted = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+        cntHeight = 0
+        biggestHatch = []
+        for cnt in cntsSorted:
+            x, y, w, h = cv2.boundingRect(cnt)
+            cntHeight = h
+            aspect_ratio = float(w) / h
+            # Get moments of contour; mainly for centroid
+            M = cv2.moments(cnt)
+            # Get convex hull (bounding polygon on contour)
+            hull = cv2.convexHull(cnt)
+            # Calculate Contour area
+            cntArea = cv2.contourArea(cnt)
+            # Filters contours based off of size
+            if (checkHatch(cntArea, aspect_ratio)):
+                ### MOSTLY DRAWING CODE, BUT CALCULATES IMPORTANT INFO ###
+                # Gets the centeroids of contour
+                if M["m00"] != 0:
+                    cx = int(M["m10"] / M["m00"])
+                    cy = int(M["m01"] / M["m00"])
+                else:
+                    cx, cy = 0, 0
+                if (len(biggestHatch) < 3):
+
+                    ##### DRAWS CONTOUR######
+                    # Gets rotated bounding rectangle of contour
+                    rect = cv2.minAreaRect(cnt)
+                    # Creates box around that rectangle
+                    box = cv2.boxPoints(rect)
+                    # Not exactly sure
+                    box = np.int0(box)
+                    # Draws rotated rectangle
+                    cv2.drawContours(image, [box], 0, (23, 184, 80), 3)
+
+                    # Draws a vertical white line passing through center of contour
+                    cv2.line(image, (cx, screenHeight), (cx, 0), (255, 255, 255))
+                    # Draws a white circle at center of contour
+                    cv2.circle(image, (cx, cy), 6, (255, 255, 255))
+
+                    # Draws the contours
+                    cv2.drawContours(image, [cnt], 0, (23, 184, 80), 1)
+
+                    # Gets the (x, y) and radius of the enclosing circle of contour
+                    (x, y), radius = cv2.minEnclosingCircle(cnt)
+                    # Rounds center of enclosing circle
+                    center = (int(x), int(y))
+                    # Rounds radius of enclosning circle
+                    radius = int(radius)
+                    # Makes bounding rectangle of contour
+                    rx, ry, rw, rh = cv2.boundingRect(cnt)
+
+                    # Draws countour of bounding rectangle and enclosing circle in green
+                    cv2.rectangle(image, (rx, ry), (rx + rw, ry + rh), (23, 184, 80), 1)
+
+                    cv2.circle(image, center, radius, (23, 184, 80), 1)
+
+                    # Appends important info to array
+                    if [cx, cy, cnt, cntHeight] not in biggestHatch:
+                        biggestHatch.append([cx, cy, cnt, cntHeight])
+
+        # Check if there are cargo seen
+        if (len(biggestHatch) > 0):
+            # pushes that it sees cargo to network tables
+            networkTable.putBoolean("hatchDetected", True)
+            finalTarget = []
+            # Sorts targets based on x coords to break any angle tie
+            biggestHatch.sort(key=lambda x: math.fabs(x[0]))
+            closestCargo = min(biggestHatch, key=lambda x: (math.fabs(x[0] - centerX)))
+            xCoord = closestCargo[0]
+            finalTarget.append(calculateYaw(xCoord, centerX, H_FOCAL_LENGTH))
+            finalTarget.append(calculateDistWPILib(closestCargo[3]))
+            print("Yaw: " + str(finalTarget[0]))
+            networkTable.putString("Yaw", finalTarget[0])
+            networkTable.putString("Dist:", finalTarget[1])
+            # Puts the yaw on screen
+            # Draws yaw of target + line where center of target is
+            cv2.putText(image, "Yaw: " + str(finalTarget[0]), (40, 40), cv2.FONT_HERSHEY_COMPLEX, .6,
+                        (255, 255, 255))
+            cv2.putText(image, "Dist: " + str(finalTarget[1]), (40, 100), cv2.FONT_HERSHEY_COMPLEX, .6,
+                        (255, 255, 255))
+            cv2.line(image, (xCoord, screenHeight), (xCoord, 0), (255, 0, 0), 2)
+
+            currentAngleError = finalTarget[0]
+            # pushes cargo angle to network tables
+            networkTable.putNumber("hatchYaw", currentAngleError)
+            networkTable.putNumber("hatchDist", finalTarget[1])
+
+        else:
+            # pushes that it doesn't see cargo to network tables
+            networkTable.putBoolean("hatchDetected", False)
 
         cv2.line(image, (round(centerX), screenHeight), (round(centerX), 0), (255, 255, 255), 2)
 
@@ -547,6 +672,9 @@ def checkContours(cntSize, hullSize):
 
 # Checks if ball contours are worthy based off of contour area and (not currently) hull area
 def checkBall(cntSize, cntAspectRatio):
+    return (cntSize > (image_width / 2)) and (round(cntAspectRatio) == 1)
+
+def checkHatch(cntSize, cntAspectRatio):
     return (cntSize > (image_width / 2)) and (round(cntAspectRatio) == 1)
 
 
@@ -800,8 +928,9 @@ if __name__ == "__main__":
     # TOTAL_FRAMES = 200;
     # loop forever
     networkTable.putBoolean("Driver", False)
-    networkTable.putBoolean("Tape", True)
+    networkTable.putBoolean("Tape", False)
     networkTable.putBoolean("Cargo", False)
+    networkTable.putBoolean("Hatch", False)
     networkTable.putBoolean("WriteImages", True)
     networkTable.putBoolean("SendMask", False)
     networkTable.putBoolean("TopCamera", True)
@@ -866,6 +995,17 @@ if __name__ == "__main__":
                     # cv2.putText(frame, "Find Cargo", (40, 40), cv2.FONT_HERSHEY_COMPLEX, .6, (255, 255, 255))
                     threshold = threshold_video(lower_orange, upper_orange, boxBlur)
                     processed = findCargo(frame, threshold)
+                elif (networkTable.getBoolean("Hatch", True)):
+                    # Checks if you just want camera for Cargo processing, by dent of everything else being false, true by default
+                    #if (networkTable.getBoolean("Cargo", True)):
+                    if switch != 3:
+                        print("find hatch")
+                        switch = 3
+                    cap.autoExpose = True
+                    boxBlur = blurImg(frame, yellow_blur)
+                    # cv2.putText(frame, "Find Cargo", (40, 40), cv2.FONT_HERSHEY_COMPLEX, .6, (255, 255, 255))
+                    threshold = threshold_video(lower_yellow, upper_yellow, boxBlur)
+                    processed = findHatch(frame, threshold)
 
         # Puts timestamp of camera on netowrk tables
         networkTable.putNumber("VideoTimestamp", timestamp)
